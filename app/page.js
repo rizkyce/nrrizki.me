ef5437e3a30e2c85207f1eef21b4b0edd446ef46
Rizky <rizkycyrf05@gmail.com>
2023-11-25 15:12:57 +0700

    Landing Page

--- app/page.js
+++ app/page.js
@@ -1,11 +1,12 @@
-import Image from "next/image";
+import Hero from "@/components/Hero/page";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-2xl">
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      {/* <h1 className="text-2xl">
         Welcome To <span className="font-bold text-blue-500">nrrizki.me</span>
-      </h1>
+      </h1> */}
+      <Hero />
     </main>
   );
 }
---
+++ components/Hero/textAnimation.jsx
@@ -0,0 +1,20 @@
+import { motion } from "framer-motion";
+
+const TextAnimation = () => {
+  const text = "Nur Ridho Rizki";
+  return (
+    <motion.div className="text">
+      {text.split("").map((letter, index) => (
+        <motion.span
+          key={index}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.2, delay: index * 0.1 }}
+        >
+          {letter}
+        </motion.span>
+      ))}
+    </motion.div>
+  );
+};
+export default TextAnimation;
