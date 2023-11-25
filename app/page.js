d1648afd1a4558b6b88b61cb0ba76424026aae40
Rizky <rizkycyrf05@gmail.com>
2023-11-25 20:01:01 +0700

    fix hero section

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import Hero from "@/components/Hero/page";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
+    <main className="">
       {/* <h1 className="text-2xl">
         Welcome To <span className="font-bold text-blue-500">nrrizki.me</span>
       </h1> */}
